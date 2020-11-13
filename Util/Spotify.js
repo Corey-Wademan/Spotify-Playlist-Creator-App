f8e84aa27b19f5e2fc587f5a3405db1658766d75
Corey Wademan <67563215+Corey-Wademan@users.noreply.github.com>
2020-11-13 09:21:21 -0500

    Add files via upload

---
+++ Util/Spotify.js
@@ -0,0 +1,30 @@
+let accessToken;
+const clientId = '67d33ca364394930b2b03666322f62b0';
+const redirectURI = 'http://localhost:3000'
+
+const Spotify = {
+    getAccessToken() {
+        if (accessToken) {
+            return accessToken;
+        }
+
+        // check for access token match
+        const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+        const expirationMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+        if (accessTokenMatch && expirationMatch) {
+            accessToken = accessTokenMatch[1];
+            const expiresIn = Number(expirationMatch[1]);
+
+            // clears parameters allowing us to grab a new access token when it expires
+            window.setTimeout(() => accessToken = '', expiresIn * 1000);
+            window.history.pushState('Access Token', null, '/');
+            return accessToken;
+        } else {
+            const accessURL = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`;
+            window.location = accessURL;
+        }
+    }
+}
+
+export default Spotify
