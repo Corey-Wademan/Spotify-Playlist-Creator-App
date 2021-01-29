5c3df8039c25bca1fce5d133f5719928b9cdc857
Corey Wademan <67563215+Corey-Wademan@users.noreply.github.com>
2021-01-28 22:18:11 -0500

    Add files via upload

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -71,7 +71,7 @@ export default class App extends React.Component {
     render() { 
         return (
             <div>
-                <h1>Gr<span className='highlight'>ooo</span>vin Spotify Playlist Creator</h1>
+                <h1><span className='highlight'>Spotify</span>Playlist Creator</h1>
                 <div className="App">
                 <SearchBar 
                     onSearch={this.search}
